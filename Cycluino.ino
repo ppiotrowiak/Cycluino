a66e747c805c7d6be41c0aedd46ba6708ec2dbd8
ppiotrowiak <p.piotrowiak@yahoo.ie>
2017-05-01 21:11:34 +0100

    dołączenie bibliotek czesc 1
    dolacz biblioteki - proba devl

--- Cycluino.ino
+++ Cycluino.ino
@@ -7,7 +7,13 @@
  * Wersja dla Arduino Mega2560
  */
  
-//Dodaj biblioteki
+//Dolacz konieczne biblioteki
+#include <Wire.h> // Komunikacja przez protokół I2C / TWI
+#include <SPI.h> // Do komunikacji z urządzeniami przez interfejs SPI (Serial Peripheral Interface)
+#include <Adafruit_GFX.h>  // Biblioteka graficzna stworzona przez Adafruit
+#include <Adafruit_ST7735.h> // Biblioteka dla kontrolera ekranu TFT stworzona przez Adafruit
+#include <SD.h> // Biblioteka do obsługi karty SD
+#include "Encoder.h" // Wlasna biblioteka dla enkodera
  
 void setup() {
   // put your setup code here, to run once:
