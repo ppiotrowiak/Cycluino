3237e2ea97334187ee669d02d822029a6be770cb
ppiotrowiak <p.piotrowiak@yahoo.ie>
2017-05-01 16:17:09 +0100

    Dodaj Cycluino
    Główny plik projektu

---
+++ Cycluino.ino
@@ -0,0 +1,20 @@
+/*
+ * Cycluino - Komputer rowerowy w oparciu o mikrokontroler Arduino - Praca inżynierska
+ * Politechnika Warszawska
+ * Informatyka Stosowana
+ * Wydział Elektryczny
+ * Autor: Przemyslaw Piotrowiak
+ * Wersja dla Arduino Mega2560
+ */
+ 
+//Dodaj biblioteki
+ 
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
